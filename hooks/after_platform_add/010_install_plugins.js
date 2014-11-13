274b2b77163a99ae8cc5906600659c24461adec4
Leah Jones <leah@l12s.com>
2014-11-12 16:45:56 -0800

    Tweaks the install plugins hook to get the local notifications plugin working

--- hooks/after_platform_add/010_install_plugins.js
+++ hooks/after_platform_add/010_install_plugins.js
@@ -23,7 +23,8 @@ var installPlugins = function(installedPlugins) {
   for (var key in plugins) {
     if (installedPlugins.indexOf(key) === -1) {
       pluginName = plugins[key];
-      exec('cordova plugin add '+ pluginName, function(error, stdout, stderr) {
+      // the prepare each time is required to ensure the local notification plugin works correctly
+      exec('cordova plugin add '+ pluginName + ' && cordova prepare', function(error, stdout, stderr) {
         sys.print(stdout);
       });
     }
