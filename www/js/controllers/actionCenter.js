d6a3e2a1dd9a3687bd7a2b71078dcca7c2206d37
Paul Andrew <pauljandrew@gmail.com>
2015-10-27 23:04:16 -0600

    Add base code for options popover. Issue #27

--- www/js/controllers/actionCenter.js
+++ www/js/controllers/actionCenter.js
@@ -2,10 +2,20 @@
  * Parent controller.
  */
 
-var ActionCenterCtrl = function($scope) {
+var ActionCenterCtrl = function($scope, $ionicPopover) {
+
+  $ionicPopover.fromTemplateUrl('templates/optionsPopover.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
 
   $scope.goToUrl = function(url) {
     window.open(url, '_system', 'location=yes');
+  }
+
+  $scope.openOptionsPopover = function($event){
+    $scope.popover.show($event);
   }
 
 };
