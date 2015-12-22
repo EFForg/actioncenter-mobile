9687391e095d83cecab8d23c742408ba7076f2ce
mark burdett <mfb@eff.org>
2015-12-21 17:46:55 -0800

    fixes #36 HTML-decoding of RSS content

--- www/js/app.js
+++ www/js/app.js
@@ -52,6 +52,10 @@ actionCenterMobile.config(function ($ionicConfigProvider) {
     .style('striped');
 });
 
+actionCenterMobile.config(function (x2jsProvider) {
+  x2jsProvider.config = { escapeMode: false };
+});
+
 actionCenterMobile.controller('ActionCenterCtrl', require('./controllers/actionCenter'));
 actionCenterMobile.controller('WelcomeCarouselCtrl', require('./controllers/welcome_carousel'));
 actionCenterMobile.controller('ActionCtrl', require('./controllers/action'));
