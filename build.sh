1751a1d42e012d335a499fbd740279664b32925a
Peter Woo <peterw@eff.org>
2017-12-11 16:56:45 -0800

    Create Dockerfile + scripts for dev/release building

---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+GULP=0
+SIGN=0
+KEYSTORE_FILE=
+KEYSTORE_ALIAS=
+
+while [ $# -ne 0 ]; do
+    case $1 in
+        --gulp)
+            GULP=1
+            shift
+            ;;
+
+        --sign)
+            SIGN=1
+            KEYSTORE_FILE=$(echo "$2" | cut -d : -f 1)
+            KEYSTORE_ALIAS=$(echo "$2" | cut -d : -f 2)
+            shift 2
+            ;;
+
+        *)
+            echo "Usage: $0 [--gulp] [--sign alias]"
+            exit 1
+            ;;
+
+    esac
+done
+
+if [ $GULP -eq 1 ]; then
+    cordova clean
+
+    npm install
+    gulp build
+fi
+
+export NODE_ENV=production
+cordova build --release android
+
+cp platforms/android/build/outputs/apk/release/android-release-unsigned.apk android-release-unsigned.apk
+
+if [ $SIGN -eq 1 ]; then
+    jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
+              -keystore "$KEYSTORE_FILE" \
+              android-release-unsigned.apk \
+              "$KEYSTORE_ALIAS"
+fi
+
+zipalign -f -v 4 android-release-unsigned.apk releases/android-release.apk
+
